b3a32c610467d8422a5e48c89cff5ed3c1a9ad50
s4ng <zalcls951201@gmail.com>
2019-07-11 22:44:29 +0900

    0711 / final commit

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "sang", age = 25, gender = "male";
-const sayHI = (name, age, gender) => {
-    console.log(`Hi ${name}, you are ${age} and you are a ${gender}`);
-};
-sayHI(name, age, gender);
-//# sourceMappingURL=index.js.map
--- index.ts
+++
@@ -1,9 +0,0 @@
-const name = "sang",
-age = 25,
-gender = "male";
-
-const sayHI = (name, age, gender) => {
-    console.log(`Hi ${name}, you are ${age} and you are a ${gender}`)
-}
-sayHI(name, age, gender);
-export {};
